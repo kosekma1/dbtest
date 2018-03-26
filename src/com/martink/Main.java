8ffc79236846b985ae67d56ca6b58fad1ef2727c
Martin Kosek <kosek.martin@gmail.com>
2018-03-26 23:55:51 +0200

    Database - ResultSet

--- src/com/martink/Main.java
+++ src/com/martink/Main.java
@@ -1,9 +1,6 @@
 package com.martink;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -17,11 +14,26 @@ public class Main {
         try {
             //Class.forName("org.sql.JDBC"); used to use
             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Programy\\sqlitedb\\testjava.db");
-            conn.setAutoCommit(false);
+//            conn.setAutoCommit(false); //default behavior is autocommit true
             Statement statement = conn.createStatement();
 
             statement.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)");
-            statement.execute("INSERT INTO contacts (name, phone, email) VALUES ('Joe', 852652553, 'joe@gmail.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES ('Joe', 852652553, 'joe@gmail.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES ('Jane', 4654654, 'jane@gmail.com')");
+//            statement.execute("INSERT INTO contacts (name, phone, email) VALUES ('Fido', 321, 'fido@gmail.com')");
+//            statement.execute("UPDATE contacts SET phone=1234 WHERE name='Jane'");
+//            statement.execute("DELETE FROM contacts WHERE name='Joe'");
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet results = statement.getResultSet();
+            while(results.next()){
+                System.out.println(results.getString("name") + " " +
+                                                      results.getInt("phone") + " " +
+                                                              results.getString("email")
+                );
+            }
+
+            results.close();
 
             //right order - statement first then conn to close
             statement.close();
